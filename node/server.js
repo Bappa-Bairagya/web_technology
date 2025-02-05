51b4c130719f0d1dfbf07b5c92ba8dc3c03c8eec
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-02-05 12:45:11 +0530

    practice

--- node/server.js
+++ node/server.js
@@ -1,12 +1,22 @@
-const fs=require("fs")
-//console.log(fs);
-//fs.writeFileSync("b.txt","helloi")
-fs.writeFile("a.txt", "hi i am a",()=>{
-    console.log("done");
-    fs.readFile("a.txt",(error,read)=>{
-console.log(error,read.toString());
-
-    })
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('song'))
+app.get('/', (req, res) => {
+  res.send('Hello World1!')
 })
-
-
+app.get('/ho', (req, res) => {
+  res.send('home')
+})
+app.get('/about', (req, res) => {
+  console.log(req);
+  
+  res.send('about')
+})
+// app.get('/blog/:slug/:se', (req, res) => {
+//   res.send(`Hello ${req.params.slug} and ${req.params.se}`)
+// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+      
