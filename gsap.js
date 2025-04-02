d37a63bb1f08ca62cb361ee6a5dba0bdefc28537
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-04-02 10:59:57 +0530

    practice

--- gsap.js
+++ gsap.js
@@ -69,3 +69,23 @@ gsap.to("#page2 h1",{
         pin :true
     }
 })
+var path=`M 10 300 Q 700 300 1450 300`
+var finalpath=`M 10 300 Q 700 300 1450 300`
+var page=document.querySelector("#page3")
+page.addEventListener("mousemove",function(gets){
+    console.log(gets.y);
+    path=`M 10 300 Q 700 ${gets.y} 1450 300`
+    gsap.to("#page3 svg path",{
+        attr:{d:path},
+        duration:0.2
+    })
+})
+page.addEventListener("mouseleave",function(gets){
+    
+    
+    gsap.to("#page3 svg path",{
+        attr:{d:finalpath},
+        duration:1.5,
+        ease: "elastic.out(1,0.1)",
+    })
+})
