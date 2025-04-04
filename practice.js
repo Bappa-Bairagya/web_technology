0d3ae0b0da01d077fab6c9b4bbfd435c64ae3ecc
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-04-04 14:13:47 +0530

    practice

--- practice.js
+++ practice.js
@@ -23,10 +23,37 @@ cross.addEventListener("click",function(){
     tl.reverse()
     })
 var body=document.querySelector("#main")
-body.addEventListener("mousemove",function(data){
-console.log(data.x);
-gsap.to(".mouse",{
-    x:data.x,
-    y:data.y
-})
+// body.addEventListener("mousemove",function(data){
+// console.log(data.x);
+// gsap.to(".mouse",{
+//     x:data.x,
+//     y:data.y
+// })
+// })
+window.addEventListener("wheel",(get)=>{
+
+if(get.deltaY>0){
+    console.log("down");
+    gsap.to(".markue",{
+        transform:"translateX(-200%)",
+        duration:4,
+        repeat:-1,
+        ease:"linear"
+    })
+    gsap.to(".markue img",{
+        rotate:180
+    })
+}
+else{
+    gsap.to(".markue",{
+        transform:"translateX(0%)",
+        duration:4,
+        repeat:-1,
+        ease:"linear"
+    })
+    gsap.to(".markue img",{
+        rotate:0
+    })
+    
+}
 })
